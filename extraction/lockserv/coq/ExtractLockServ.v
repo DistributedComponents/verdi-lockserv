5be20fe85450d5a7dbd2529320ad6d3f12382f3e
Karl Palmskog <palmskog@gmail.com>
2017-02-27 20:37:35 -0600

    remove unnecessary imports for extraction

--- extraction/lockserv/coq/ExtractLockServ.v
+++ extraction/lockserv/coq/ExtractLockServ.v
@@ -6,8 +6,6 @@ Require Import ExtrOcamlBasic.
 Require Import ExtrOcamlNatInt.
 
 Require Import Verdi.ExtrOcamlBasicExt.
-Require Import Verdi.ExtrOcamlNatIntExt.
-
 Require Import Verdi.ExtrOcamlList.
 Require Import Verdi.ExtrOcamlFin.
 
