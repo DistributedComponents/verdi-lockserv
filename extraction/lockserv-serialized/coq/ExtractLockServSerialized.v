ca52acfeab88a53f6a946ba9e8126aff8c7cd7b9
Karl Palmskog <palmskog@gmail.com>
2017-07-12 00:44:34 -0500

    adapt to extraction naming convention

--- extraction/lockserv-serialized/coq/ExtractLockServSerialized.v
+++ extraction/lockserv-serialized/coq/ExtractLockServSerialized.v
@@ -13,8 +13,7 @@ Require Import ExtrOcamlString.
 Require Import Verdi.ExtrOcamlBasicExt.
 Require Import Verdi.ExtrOcamlList.
 Require Import Verdi.ExtrOcamlFin.
-
-Require Import Cheerios.ExtractCheerios.
+Require Import Cheerios.ExtrCheerios.
 
 Extract Inlined Constant nat_serialize => "(fun i -> Serializer_primitives.putInt (Int32.of_int i))".
 Extract Inlined Constant nat_deserialize => "(Serializer_primitives.map Int32.to_int Serializer_primitives.getInt)".
